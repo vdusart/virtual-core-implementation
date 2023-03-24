8004f6e78c1e074c0dc538bea4b2c1b3c9cfe27b
Victor DUSART <dusartvictor@gmail.com>
2023-03-24 16:50:56 +0100

    add fetch function

--- virtual-core/src/main.rs
+++ virtual-core/src/main.rs
@@ -1,5 +1,7 @@
 use std::{env, process, collections::HashMap};
 
+use keywords::BranchingCodes;
+
 mod executor;
 mod keywords;
 mod loading;
@@ -30,7 +32,7 @@ fn execute_instruction(instruction: u32, registers: &mut Vec<i64>, flags: &mut H
             Ok(keywords::OperationCodes::EOR) => executor.eor(ope1, ope2, dest),
             Ok(keywords::OperationCodes::ADD) => executor.add(ope1, ope2, dest, carry_flag),
             Ok(keywords::OperationCodes::ADC) => executor.adc(ope1, ope2, dest, carry_flag),
-            Ok(keywords::OperationCodes::CMP) => executor.cmp(ope1, ope2, dest),
+            Ok(keywords::OperationCodes::CMP) => executor.cmp(ope1, ope2, flags),
             Ok(keywords::OperationCodes::SUB) => executor.sub(ope1, ope2, dest, carry_flag),
             Ok(keywords::OperationCodes::SBC) => executor.sbc(ope1, ope2, dest, carry_flag),
             Ok(keywords::OperationCodes::MOV) => executor.mov(ope2, dest),
@@ -42,12 +44,41 @@ fn execute_instruction(instruction: u32, registers: &mut Vec<i64>, flags: &mut H
         println!("r2 = {0}", registers[2]);
         println!("r3 = {0}", registers[3]);
         println!("carry = {0}", flags.get(&String::from("carry")).unwrap());
+        println!("BNE = {0}", flags.get(&String::from("BNE")).unwrap());
 
 
     } else {
         println!("--- BCC ---");
     }
     println!("---------------");
+}
+
+fn fetch(instruction: u32, pc: u32, flags: &mut HashMap<String, bool>, is_verbose: bool) -> (bool, u32) {
+    let bcc = (instruction >> 28) & 0b1111;
+    let mut offset: i32 = 0;
+    if bcc != 0 {
+        let branching_opcode: BranchingCodes = bcc.try_into().unwrap();
+        let string = branching_opcode.to_string();
+        if bcc == 8 || *flags.get(&string).unwrap() {
+            let sign_bit = instruction >> 27 & 0b1;
+            offset = (-1 as i32).pow(sign_bit) * (instruction & 0x7ffffff) as i32;
+        } else {
+            offset += 1;
+        }
+    } else {
+        offset += 1;
+    }
+    let (new_pc, _) = pc.overflowing_add_signed(offset);
+
+    if is_verbose {
+        println!("------- fetch -----");
+        println!("\tBCC: {:02x}", bcc);
+        println!("\toffset: {offset}");
+        println!("\tinstruction: {:032b}", instruction);
+        println!("\tNew PC: {new_pc}");
+    }
+
+    return (bcc != 0, new_pc)
 }
 
 fn main() {
@@ -71,11 +102,22 @@ fn main() {
     // Load instructions
     let mut instructions: Vec<u32> = Vec::new();
     loading::load_binary_file(args[1].to_string(), &mut instructions);
-    for instruction in instructions.iter() {
-        // println!("{:032b}", instruction);
-        // fetch
-        // decode
-        execute_instruction(*instruction, &mut registers, &mut flags);
+
+    let mut pc: u32 = 0;
+    let is_verbose: bool = true;
+    let instructions_size = instructions.len().try_into().unwrap();
+    while pc < instructions_size {
+        println!("\n-------------");
+        let instruction = instructions[pc as usize];
+        println!("Current PC: {pc}");
+        let (is_bcc, new_pc) = fetch(instruction, pc, &mut flags, is_verbose);
+        if !is_bcc {
+            // decode
+            // execute
+            execute_instruction(instruction, &mut registers, &mut flags);
+        }
+        pc = new_pc;
+        println!("-------------\n");
     }
     for (i, r) in registers.iter().enumerate() {
         println!("r{:#02} = {:#018x}", i, r);
