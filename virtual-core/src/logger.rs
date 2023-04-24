e02194b7c72591d0f529630104c29fde15e0614e
Victor DUSART <dusartvictor@gmail.com>
2023-04-24 14:12:30 +0200

    apply suggestions

--- virtual-core/src/logger.rs
+++ virtual-core/src/logger.rs
@@ -1,12 +1,16 @@
 pub struct Logger;
 
 impl Logger {
+    const ERROR: &str = "\x1b[0;31m";
+    const INFO: &str = "\x1b[0;34m";
+    const RESET: &str = "\x1b[0m";
+
     pub fn info(msg: &str) {
-        println!("\x1b[0;34mInfo: {msg}\x1b[0m");
+        println!("{}[INFO]{} {}", Logger::INFO, Logger::RESET, msg);
     }
 
     pub fn error(msg: &str) {
-        eprintln!("\x1b[0;31mError: {msg}\x1b[0m");
+        eprintln!("{}[ERROR]{} {}", Logger::ERROR, Logger::RESET, msg);
         std::process::exit(-1);
     }
 }
