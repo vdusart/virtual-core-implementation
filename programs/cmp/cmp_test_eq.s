cb11eb84b9cbcefbc5b2f88c3694da74edddd9c9
Victor DUSART <dusartvictor@gmail.com>
2023-04-25 14:38:28 +0200

    add cmp program explanation

--- programs/cmp/cmp_test_eq.s
+++ programs/cmp/cmp_test_eq.s
@@ -3,6 +3,8 @@
 ;     - First function parameter: r0.
 ;     - Second function parameter: r1.
 ;     - Function return value: r2.
+; Explanation
+; This program puts the '0xfe' value in r2 (the return register) and only changes its value (to '0xaf') when A is greater than B.
 MOV r0, 49
 MOV r1, 49
 ; int compare(int a, int b)
