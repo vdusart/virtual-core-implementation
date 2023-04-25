d850bfe774d1d9441b5651d7a4a3b0eb409c6e06
Victor Dusart <43795504+vdusart@users.noreply.github.com>
2023-04-25 14:26:28 +0200

    Update programs/cmp/cmp_test_sup.s
    Co-authored-by: Lucas DRAESCHER <68022925+draescherl@users.noreply.github.com>

--- programs/cmp/cmp_test_sup.s
+++ programs/cmp/cmp_test_sup.s
@@ -1,4 +1,8 @@
-; A > B
+; This function returns 0xfe because A > B.
+; Conventions used for this function:
+;     - First function parameter: r0.
+;     - Second function parameter: r1.
+;     - Function return value: r2.
 MOV r0, 49
 MOV r1, 21
 ; int compare(int a, int b)
