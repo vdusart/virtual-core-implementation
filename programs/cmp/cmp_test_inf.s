cbcf24cbaf1fb3e3f3c987eb786a45fa34ec64de
Victor Dusart <43795504+vdusart@users.noreply.github.com>
2023-04-25 14:26:12 +0200

    Update programs/cmp/cmp_test_inf.s
    Co-authored-by: Lucas DRAESCHER <68022925+draescherl@users.noreply.github.com>

--- programs/cmp/cmp_test_inf.s
+++ programs/cmp/cmp_test_inf.s
@@ -1,4 +1,8 @@
-; A < B
+; This function returns 0xaf because A < B.
+; Conventions used for this function:
+;     - First function parameter: r0.
+;     - Second function parameter: r1.
+;     - Function return value: r2.
 MOV r0, 49
 MOV r1, 215
 ; int compare(int a, int b)
